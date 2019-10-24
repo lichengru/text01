aab2e0b460a14a2be06f1e95785bd09c35600550
lichengru <gjo4us06c961115@gmail.com>
2019-10-24 19:08:37 +0800

    text001

--- test01/Program.cs
+++ test01/Program.cs
@@ -8,33 +8,38 @@ namespace test01
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int GetInput()
         {
-            int input = -1;
-            int guess = 55;
-
-            //產生亂數
-            guess = new Random().Next(100);
-            Console.WriteLine("請輸入一個數字(0-100):");
-
-            while (true)  //game loop 
-            {
-                //決定輸入內容
-                string input_str = Console.ReadLine();  //input  string 
-                input = int.Parse(input_str);
-
-                //離開條件 
-                if (input == guess)
-                {
-                    Console.WriteLine("恭喜你答對了!");
-                    break;
-                }
-                //提示
-                Console.WriteLine("猜錯了，再來一次！");
-            }
-
-            Console.WriteLine("PAUSE");
-            Console.ReadKey();
-        }
+        string intput_str = Console.ReadLine();
+            return int.Parse(intput_str);
     }
+    static void Main(string[] args)
+    {
+        int input = -1;
+        int guess = 55;
+
+        //產生亂數
+        guess = new Random().Next(100);
+        Console.WriteLine("請輸入一個數字(0-100):");
+
+        while (true)  //game loop 
+        {
+            //決定輸入內容
+            //string input_str = Console.ReadLine();  //input  string 
+            input = GetInput();
+
+            //離開條件 
+            if (input == guess)
+            {
+                Console.WriteLine("恭喜你答對了!");
+                break;
+            }
+            //提示
+            Console.WriteLine("猜錯了，再來一次！");
+        }
+
+        Console.WriteLine("PAUSE");
+        Console.ReadKey();
+    }
+}
 }
