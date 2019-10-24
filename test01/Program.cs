6d5bdbd5400daeaf0cff2f65f0852fdb9d786fd9
lichengru <gjo4us06c961115@gmail.com>
2019-10-24 19:29:14 +0800

    test02

--- test01/Program.cs
+++ test01/Program.cs
@@ -8,16 +8,38 @@ namespace test01
 {
     class Program
     {
-        public static int GetInput()
+        public static int GetInput(int min, int max)
         {
-        string intput_str = Console.ReadLine();
-            return int.Parse(intput_str);
-    }
+            int result = -1;
+
+            while (true)
+            {
+                string input_str = Console.ReadLine();
+
+                if (int.TryParse(input_str, out result))
+                {
+                    result = int.Parse(input_str);
+
+                    //離開的條件
+                    if (result >= min && result <= max)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("您輸入的數字不在範圍內，請重新輸入一個數字(0-100):");
+                }
+                else
+                {
+                    //輸入的不是數字
+                    Console.WriteLine("您輸入的不是數字，請重新輸入一個數字(0-100):");
+                }
+            }
+            return result;
+        }
+    
     static void Main(string[] args)
     {
         int input = -1;
         int guess = 55;
-
         //產生亂數
         guess = new Random().Next(100);
         Console.WriteLine("請輸入一個數字(0-100):");
@@ -26,10 +48,10 @@ namespace test01
         {
             //決定輸入內容
             //string input_str = Console.ReadLine();  //input  string 
-            input = GetInput();
+            input = GetInput(0,100);
 
             //離開條件 
-            if (input == guess)
+            if (input==guess)
             {
                 Console.WriteLine("恭喜你答對了!");
                 break;
